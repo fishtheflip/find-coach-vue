a9133ea38c6c070f3afbd52f14669779cd0c386e
Andrei Parkhomenko <ataflava@gmail.com>
2021-08-21 16:43:24 +0600

    first commit

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        DETAIL FOR COACH
+        <router-view></router-view>
+        <router-link to="/coaches/c1/contact">Contact</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <section>Filter</section>
+        <section>
+            <div class="controls">
+                <button>Refresh</button>
+                <router-link to="/register">Register as Coach</router-link>
+            </div>
+            <ul>List of Coaches</ul>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
